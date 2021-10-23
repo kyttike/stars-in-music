fb186309d84557a23facca640969d67db3aacc69
Daniel Kütt <danielkutt@hey.com>
2021-10-23 18:56:16 +0300

    Add custom song support

--- src/song-list.ts
+++ src/song-list.ts
@@ -5,10 +5,39 @@ import {GameIdentifier, GameSong, SongRepository} from "./services/SongRepositor
 export class SongList {
 
   songList: GameIdentifier[] = [];
+  encodedSongs: string = '';
 
   constructor(private songService: SongRepository){}
 
   activate() {
+    this.songList = this.songService.getSongList();
+    console.log(this.songList);
+  }
+
+  importantButton() {
+    const result = JSON.parse(atob(this.encodedSongs));
+    this.songList = result.map((x,i) => {
+      return {
+        identifier: {
+          id: i,
+          title: x.title,
+        },
+        lyrics: x.lyrics.map(lyric => lyric.text),
+        types: x.lyrics.map(lyric => lyric.trap),
+      }
+    })
+    this.songService.songs = {};
+    this.songService.createSongs(result.map((x,i) => {
+        return {
+          identifier: {
+            id: i,
+            title: x.title,
+          },
+          lyrics: x.lyrics.map(lyric => lyric.text),
+          types: x.lyrics.map(lyric => lyric.trap),
+        }
+      })
+    );
     this.songList = this.songService.getSongList();
   }
 }
